f528792d0fc30f16759790f5ada51e416d2a1b46
Johannes Wienke <languitar@semipol.de>
2015-01-07 20:21:19 +0100

    Add build system for deprecated domain
    Adds an initial version of the build system for the deprecated domain in
    all languages apart from matlab, which will be removed in the future.
    
    * CMakeLists.txt: variables for deprecated domain added
    * cpp/CMakeLists.txt:
      variables added, add subfolder, include deprecated in config file and
      pkg-config
    * cpp/RSTBuildTreeSettings.cmake.in: include deprecated domain
    * cpp/RSTConfig.cmake.in: include deprecated domain
    * cpp/deprecated/CMakeLists.txt: new build instructions for this domain
    * cpp/rstdeprecated.pc.in: include deprecated domain
    * java/CMakeLists.txt: include deprecated domain
    * java/build.xml.in: include deprecated domain
    * java/pom-deprecated.xml.in: new file for deprecated domain
    * proto/CMakeLists.txt: install deprecated proto files
    * python/CMakeLists.txt: include deprecated domain
    * python/init-hack-deprecated.py: init hack for deprecated domain
    * python/init-hack-sandbox.py: renamed to avoid clash
    * python/rst.pth.in: include deprecated domain
    * python/setup-deprecated.py.in: copied from sandbox for deprecated

---
+++ python/init-hack-deprecated.py
@@ -0,0 +1,99 @@
+import sys
+import pkgutil
+
+import rst
+import rstsandbox
+import rstdeprecated
+import logging
+
+logger = logging.getLogger("rstdeprecated")
+
+def addDeprecatedToRST():
+    def importIt(designator, error = True):
+        logger.debug("importIt called with '%s'" % designator)
+        if hasattr(designator, '__iter__'):
+            name = '.'.join(designator)
+        else:
+            name = designator
+        try:
+            logger.debug("Really importing name '%s'" % name)
+            return __import__(name, fromlist = [ '__dict__' ]) # WT?
+        except Exception, e:
+            if error:
+                raise e
+
+    # Find two things within the deprecated:
+    # + All Modules (since there should be no duplicates between rst
+    #   and rstdeprecated)
+    # + Packages which do not have a corresponding package relative to
+    #   the rst package.
+    #   E.g. rstdeprecated.communicationpatterns when there is no
+    #   rst.communicationpatterns
+    # We record errors caused by dependency problems so the caller can
+    # trigger additional iterations to resolve dependencies.
+    def findModules(root):
+        result = []
+        errors = []
+        for _, name, ispkg in pkgutil.iter_modules(root.__path__):
+            fullname = root.__name__ + '.' + name
+            if ispkg:
+                kind = 'package'
+            else:
+                kind = 'module'
+            logger.debug("Processing %s %s.%s", kind, root.__name__, name)
+            logger.debug('Importing %s', fullname)
+            try:
+                loaded = importIt(fullname)
+                logger.debug('Loaded %s', loaded)
+                proposedRstName = [ 'rst' ] + fullname.split('.')[1:]
+                logger.debug("Checking whether %s in rst exists" % proposedRstName)
+                if ispkg and importIt(proposedRstName, error = False):
+                    logger.debug('%s is a package which also exists under rst', fullname)
+                    subresult, suberrors = findModules(loaded)
+                    result.extend(subresult)
+                    errors.extend(suberrors)
+                else:
+                    result.append(loaded)
+            except Exception, e:
+                errors.append((name, "%s: %s" % (type(e).__name__, e)))
+                logger.warn('Failed to load %s: %s', name, e)
+        return result, errors
+
+    # Process packages and modules that are present in the deprecated,
+    # but not in the stable section:
+    # + Add package or module to respective parent package. This
+    #   corresponds to "mounting" the entire module tree rooted at the
+    #   particular package under the parent package in the stable
+    #   section.
+    # + Add to sys.modules
+    modules, errors = findModules(rstdeprecated)
+    logger.debug("Found modules %s and errors %s" % (modules, errors))
+    for module in modules:
+
+        logger.debug("Processing module or package %s" % module)
+
+        components = module.__name__.split('.')
+        rstName    = 'rst.' + '.'.join(components[1:])
+        logger.debug('Mounting %s -> %s', module.__name__, rstName)
+
+        sys.modules[rstName] = module
+        rstPackage = importIt([ 'rst' ] + components[1:-1])
+        rstPackage.__dict__[components[-1]] = module
+
+
+    return modules, errors
+
+# We need multiple iterations to resolve dependencies since deprecated
+# types may depend on each other. Since we do not attempt to load the
+# modules in a dependency-respecting order, some modules may initially
+# fail to load but succeed later, after their dependencies have been
+# loaded.
+logger.debug("---- ITERATION ----")
+_, initial = addDeprecatedToRST()
+while len(initial) > 0:
+    logger.debug("---- ITERATION ----")
+    _, new = addDeprecatedToRST()
+    if len(new) == len(initial):
+        raise RuntimeError, "Unable to install modules/packages in rst namespace:\n%s" \
+            % '\n'.join([("%s - %s" % (e[0], e[1])) for e in new])
+    initial = new
